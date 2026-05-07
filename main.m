6f194aa4ec7598229f34fabf2cebc6f0517f9843
FilipKowalczyk67 <panfifik@gmail.com>
2026-05-07 12:26:12 +0200

    inizjalizacja sieci

--- main.m
+++ main.m
@@ -1,1 +1,18 @@
-fprintf("Labubu")
+clc;
+clear;
+
+numFeatures = 200;
+numClasses = 2;
+
+layerSizes = [numFeatures, 64, 32, numClasses];
+
+model = initializeNetwork(layerSizes);
+
+disp(size(model.W{1}));
+disp(size(model.b{1}));
+
+disp(size(model.W{2}));
+disp(size(model.b{2}));
+
+disp(size(model.W{3}));
+disp(size(model.b{3}));
